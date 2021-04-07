7302d119c18f23d9c35da7bf6616ab21ea46c97d
Finsoy <vadimka.cry.00@gmail.com>
2021-04-07 19:46:32 +0300

    feat: add func renameFiles

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,18 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const stack = [];
+  const arr = [];
+  let num = [];
+  for (let i = 0; i < names.length; i++) {
+    if (stack.includes(names[i])) {
+      arr.push(names[i]);
+      num = arr.filter((elem) => elem === names[i]);
+      stack.push(`${names[i]}(${num.length})`);
+    } else stack.push(`${names[i]}`);
+  }
+  return stack;
 }
 
 module.exports = renameFiles;
