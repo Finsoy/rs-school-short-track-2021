c47dfbbc105a12bdfd5c3040485810b391228dd2
Finsoy <vadimka.cry.00@gmail.com>
2021-04-12 21:10:13 +0300

    feat: add func getDNSStats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,45 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const obj = {};
+  const stack = [];
+  let count = 0;
+  let res = '';
+  let newRes = '';
+  const str = domains.toString().split(',');
+  const newStr = str.map((item) => item.split('.'));
+  const map = newStr.map((item) => item.flat().join('.').split('.').join(''));
+
+  const flatArr = newStr.reverse().flat();
+  flatArr.map((item) => {
+    if (!stack.includes(item)) {
+      stack.push(item);
+    }
+    return stack;
+  });
+  for (let i = stack.length - 1; i >= 0; i--) {
+    if (stack.length === 1) {
+      newRes = stack[i];
+      res = stack[i];
+    }
+    if (stack[i] === []) {
+      newRes = [];
+      res = [];
+    }
+    if (stack.length !== 1) {
+      newRes = stack[i] + newRes;
+      res += `.${stack[i]}`;
+      for (let j = 0; j < map.length; j++) {
+        if (map[j].includes(newRes)) {
+          count++;
+        }
+      }
+      obj[res] = count;
+      count = 0;
+    }
+  }
+  return obj;
 }
 
 module.exports = getDNSStats;
